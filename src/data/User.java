db22359a9672470a2f5fe16cd676e5387091d5b8
이지민 <leechimins@naver.com>
2026-06-04 15:08:51 +0900

    ✨ CryptoService: 사설키 보호_지민

--- src/data/User.java
+++ src/data/User.java
@@ -1,10 +1,10 @@
 package data;
 
 public class User {
-	public String id;
+	private String id;
 	private String hashedPassword;
-	private byte[] passwordSalt;
-	private byte[] publicKeyEncoded;	// 타인이 이 사용자에게 편지를 보낼 때 AES 키를 암호화하기 위한 용도
+	private byte[] passwordSalt;		// password 원문과 더해서 해싱 -> 사설키를 암호화 해서 encryptedPrivateKey를 만드는 용도
+	private byte[] encodedPublicKey;	// 타인이 이 사용자에게 편지를 보낼 때 AES 키를 암호화하기 위한 용도
 	private byte[] encryptedPrivateKey;	// 사용자의 패스워드 기반 대칭키로 암호화된 개인키 데이터
 	
 	public User(String id, String hashedPassword, byte[] passwordSalt, 
@@ -13,8 +13,17 @@ public class User {
     this.hashedPassword = hashedPassword;
     
     this.passwordSalt = passwordSalt;
-    this.publicKeyEncoded = publicKeyEncoded;
+    this.encodedPublicKey = publicKeyEncoded;
     this.encryptedPrivateKey = encryptedPrivateKey;
     }
-	
+
+	public String getId() { return id; }
+
+	public String getHashedPassword() { return hashedPassword; }
+
+	public byte[] getPasswordSalt() { return passwordSalt; }
+
+	public byte[] getPublicKeyEncoded() { return encodedPublicKey; }
+
+	public byte[] getEncryptedPrivateKey() { return encryptedPrivateKey; }
 }
--- src/security/CryptoService.java
+++ src/security/CryptoService.java
@@ -5,12 +5,15 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Scanner;
 
 import javax.crypto.BadPaddingException;
@@ -21,6 +24,8 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+
+import data.User;
 
 public class CryptoService {
 
@@ -39,6 +44,9 @@ public class CryptoService {
 	private static final String ALGORITHM_P = "RSA";
 	private static final int SIZE_P = 1024;
 
+	// 싱글톤을 써야하는 이유
+	private int diaryCounter = 0;
+	
 	/*
 	 * ========================================== A. 키 생성
 	 * ==========================================
@@ -66,7 +74,7 @@ public class CryptoService {
 	 */
 
 	// 3. 편지 평문 본문을 일회성 AES 비밀키를 사용하여 암호화합니다.
-	public String encryptDiaryAndSave(String plainText, SecretKey secretKey)
+	public String encryptDiaryAndSave(String plainText, SecretKey secretKey, String sender, String reciver)
 			throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException,
 			BadPaddingException, UnsupportedEncodingException {
 
@@ -76,11 +84,12 @@ public class CryptoService {
 		byte[] byteText = plainText.getBytes("UTF-8");
 		byte[] encryptedBytes = cipher.doFinal(byteText);
 
-		String fullPath = "data/diaries/" + System.currentTimeMillis() + ".txt";
+		String fullPath = "data/diaries/" + sender + "_" + reciver + "_" + diaryCounter + ".txt";
 
 		try (FileOutputStream fos = new FileOutputStream(fullPath)) {
 			fos.write(encryptedBytes);
 			fos.flush();
+			diaryCounter++;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -89,12 +98,12 @@ public class CryptoService {
 	}
 
 	// 4. 암호화된 편지를 수신자의 복구된 AES 비밀키로 복호화합니다.
-	public String decryptLetter(String fname, SecretKey secretKey)
+	public String decryptLetter(String fullPath, SecretKey secretKey)
 			throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
 		Cipher cipher = Cipher.getInstance(ALGORITHM_S);
 		cipher.init(Cipher.DECRYPT_MODE, secretKey);
 
-		try (FileInputStream fis = new FileInputStream(fname);
+		try (FileInputStream fis = new FileInputStream(fullPath);
 				CipherInputStream cis = new CipherInputStream(fis, cipher);
 				Scanner c = new Scanner(cis)) {
 			String decryted = new String();
@@ -127,13 +136,13 @@ public class CryptoService {
 	}
 
 	// 6. [전자봉투 해독] RSA로 암호화된 AES 비밀키 바이너리를 수신자의 RSA 사설키로 복호화하여 객체로 복구합니다.
-	public SecretKey unwrapAESKey(byte[] encryptedSecretKey, PrivateKey receiverPrivateKey)
+	public SecretKey unwrapAESKey(byte[] encryptedSecretKeyBytes, PrivateKey receiverPrivateKey)
 			throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException,
 			BadPaddingException {
 		Cipher cipher = Cipher.getInstance("RSA/ECB/NoPadding");
 		cipher.init(Cipher.DECRYPT_MODE, receiverPrivateKey);
 
-		byte[] decryptedKeyBytes = cipher.doFinal(encryptedSecretKey);
+		byte[] decryptedKeyBytes = cipher.doFinal(encryptedSecretKeyBytes);
 		// 수업 시간에 배우지 않은 코드, 교수님께 질문 필요
 		SecretKey secretKey = new SecretKeySpec(decryptedKeyBytes, "AES");
 
@@ -145,58 +154,58 @@ public class CryptoService {
 	 * ==========================================
 	 */
 
-	/**
-	 * 5. [회원가입] 발급된 사용자의 RSA 사설키를 패스워드 기반 대칭키로 암호화합니다. (User 엔티티 내의
-	 * encryptedPrivateKey에 저장될 데이터를 생성합니다)
-	 *
-	 * @param privateKey 보호할 사용자의 RSA 사설키 객체
-	 * @param password   사용자가 입력한 패스워드 원문
-	 * @param salt       암호화 키 유도를 위한 솔트
-	 * @param iv         대칭 암호화용 IV
-	 * @return 암호화된 사설키 바이너리
-	 */
 	// 5. [회원가입] 발급된 사용자의 RSA 사설키를 패스워드 기반 대칭키로 암호화합니다.
-	public byte[] encryptPrivateKey(PrivateKey privateKey, String password, byte[] salt) {
-		// 1. 패스워드와 솔트를 결합하여 SHA-256 해시값 생성 (키 유도)
-		MessageDigest md = MessageDigest.getInstance("SHA-256");
-		md.update(password.getBytes("UTF-8"));
-		md.update(salt);
-		byte[] hashedKeyBytes = md.digest();
+	public byte[] encryptPrivateKey(PrivateKey privateKey, String password, User user)
+			throws NoSuchAlgorithmException, UnsupportedEncodingException, NoSuchPaddingException, InvalidKeyException,
+			IllegalBlockSizeException, BadPaddingException {
+		
+		// 1. 패스워드와 솔트를 결합하여 16Bytes 해시값 생성
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		md.update(password.getBytes());
+		md.update(user.getPasswordSalt());
+		byte[] hashSource = md.digest();
+		
+		// 2. 비밀키 생성
+		SecretKeySpec passwordBasedKey = new SecretKeySpec(hashSource, ALGORITHM_S);
 
-		// 2. 해시된 바이트 배열에서 AES-128 규격에 맞게 16바이트(128비트) 추출하여 비밀키 생성
-		byte[] aesKeyBytes = new byte[SIZE_S / 8]; // 128 / 8 = 16 bytes
-		System.arraycopy(hashedKeyBytes, 0, aesKeyBytes, 0, aesKeyBytes.length);
-		SecretKeySpec passwordBasedKey = new SecretKeySpec(aesKeyBytes, ALGORITHM_S);
-
-		// 3. IV를 사용하지 않으므로 AES 모드로 Cipher 초기화
+		// 3. 비밀키로 사설키 암호화
 		Cipher cipher = Cipher.getInstance(ALGORITHM_S);
 		cipher.init(Cipher.ENCRYPT_MODE, passwordBasedKey);
 
-		// 4. RSA 사설키 객체의 인코딩된 바이너리 데이터를 획득하여 암호화 수행
 		byte[] privateKeyBytes = privateKey.getEncoded();
 		byte[] encryptedPrivateKey = cipher.doFinal(privateKeyBytes);
 
 		return encryptedPrivateKey;
 	}
+	
+	// 6. [로그인 성공 시] DB에 저장되어 있던 암호화된 사설키 바이너리를 패스워드 기반 대칭키로 복호화하여 복구합니다.
+	public PrivateKey decryptPrivateKey(User user, String password) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidKeySpecException {
+		// 1. 패스워드와 솔트를 결합하여 16Bytes 해시값 생성
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		md.update(password.getBytes());
+		md.update(user.getPasswordSalt());
+		byte[] hashSource = md.digest();
+		
+		// 2. 비밀키 생성
+		SecretKeySpec passwordBasedKey = new SecretKeySpec(hashSource, ALGORITHM_S);
 
-	/**
-	 * 6. [로그인 성공 시] 가상 DB에 저장되어 있던 암호화된 사설키 바이너리를 패스워드 기반 대칭키로 복호화하여 복구합니다.
-	 *
-	 * @param encryptedPrivateKey 암호화된 사설키 바이너리 데이터
-	 * @param password            사용자가 입력한 패스워드 원문
-	 * @param salt                유저 엔티티에 저장되어 있던 솔트
-	 * @param iv                  유저 엔티티에 저장되어 있던 IV
-	 * @return 복구된 자바 PrivateKey 객체 컨텍스트
-	 */
-	public PrivateKey decryptPrivateKey(byte[] encryptedPrivateKey, String password, byte[] salt, byte[] iv)
-			throws Exception {
-		// 본문 구현 예정
-		return null;
+		// 3. 비밀키로 사설키 복호화
+		Cipher cipher = Cipher.getInstance(ALGORITHM_S);
+		cipher.init(Cipher.DECRYPT_MODE, passwordBasedKey);
+
+		byte[] decryptedKeyBytes = cipher.doFinal(user.getEncryptedPrivateKey());
+		
+		// 4. 복호화한 배열을 사설키로 복구
+		PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(decryptedKeyBytes);
+	    KeyFactory keyFactory = KeyFactory.getInstance(ALGORITHM_P);
+	    PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
+	    
+	    return privateKey;
 	}
 
-	// ==========================================
-	// E. 전자서명 생성 및 검증 (부인 방지용)
-	// ==========================================
+	/* ==========================================
+	 *  E. 전자서명 생성 및 검증 (부인 방지용)
+	 *  ========================================== */
 
 	/**
 	 * 7. 편지 내용(또는 암호문)에 대해 송신자의 RSA 사설키로 디지털 서명을 생성합니다.
